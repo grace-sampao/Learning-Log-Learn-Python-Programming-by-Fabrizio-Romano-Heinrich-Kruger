da7dc1d06abf2b1090ba78d7e339cddf7d80f780
grace-sampao <gnsampao@gmail.com>
2026-09-07 21:49:32 +0300

    Created chapter_01/scopes2.py. First commit.

---
+++ chapter_01/scopes2.py
@@ -0,0 +1,12 @@
+# Local versus Global
+
+def local():
+    # age does not belong to the scope defined by the local
+    # function so Python will keep looking into the next enclosing
+    # scope. age is finally found in the global scope.
+    print(age, 'printing from the local scope') 
+
+age = 5
+print(age, 'printing from the global scope')
+
+local()
