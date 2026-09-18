deb2b024be04a265505de2242e2395a1e427d388
grace-sampao <gnsampao@gmail.com>
2026-09-18 20:34:37 +0300

    Created chapter_03/simple.for.2.py. First commit.

---
+++ chapter_03/simple.for.2.py
@@ -0,0 +1,9 @@
+surnames = ["Rivest", "Shamir", "Adleman"]
+
+for position in range(len(surnames)):
+    print(position, surnames[position])
+
+for position in range(len(surnames)):
+    print(surnames[position][0], end="")
+
+print()
