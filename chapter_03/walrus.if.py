13e187b3ab4c0bdb794114ce72952f0bd875b645
grace-sampao <gnsampao@gmail.com>
2026-09-20 07:58:58 +0300

    Created chapter_03/walrus.if.py. First commit.

---
+++ chapter_03/walrus.if.py
@@ -0,0 +1,10 @@
+value = 13
+modulus = 5
+
+# remainder = value % modulus
+
+# if remainder:
+#     print(f"Not divisible! The remainder is {remainder}.")
+
+if remainder := value % modulus:
+    print(f"Not divisible! The remainder is {remainder}.")
